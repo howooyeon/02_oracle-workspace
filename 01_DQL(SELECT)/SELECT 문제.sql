1e956ff23d3f0f24f53aadc389272add423a0ec2
potato <aviationlovers@naver.com>
2023-06-26 20:25:43 +0900

    commit

--- 01_DQL(SELECT)/SELECT 문제.sql
+++ 01_DQL(SELECT)/SELECT 문제.sql
@@ -14,6 +14,7 @@ SET EMP_NO = '631101-1548654' --631156-1548654
 WHERE EMP_ID = '201';
 
 COMMIT;
+
 -- 1. JOB 테이블의 모든 정보 조회
 SELECT *
 FROM JOB;
@@ -81,9 +82,8 @@ WHERE (EMAIL LIKE '____$_%' ESCAPE '$') AND (DEPT_CODE = 'D9' OR DEPT_CODE ='D6'
  AND (HIRE_DATE BETWEEN '90/01/01' AND '00/12/01') AND SALARY >= 2700000;
  
 -- 15. EMPLOYEE테이블에서 사원 명과 직원의 주민번호를 이용하여 생년, 생월, 생일 조회
-SELECT EMP_NAME, EMP_ID
-FROM EMPLOYEE;
--- WHERE SUBSTR (EMP_ID, 1, 2); 
+SELECT EMP_NAME, SUBSTR(EMP_NO, 1, 2) AS 생년, SUBSTR(EMP_NO, 3, 2) AS 생월, SUBSTR(EMP_NO, 5, 2) AS 생일
+FROM EMPLOYEE; 
 
 -- 16. EMPLOYEE테이블에서 사원명, 주민번호 조회 (단, 주민번호는 생년월일만 보이게 하고, '-'다음 값은 '*'로 바꾸기)
 SELECT EMP_NAME, EMP_NO, RPAD(SUBSTR(EMP_NO,1 ,8), 14, '*')
@@ -101,16 +101,45 @@ FROM EMPLOYEE
 WHERE MOD(EMP_ID, 2) = 1;
 
 -- 19. EMPLOYEE테이블에서 근무 년수가 20년 이상인 직원 정보 조회
-
+SELECT EMP_NO, EMP_NAME, DEPT_CODE, HIRE_DATE
+FROM EMPLOYEE
+WHERE ADD_MONTHS(HIRE_DATE, 240) <= SYSDATE;
 
 -- 20. EMPLOYEE 테이블에서 사원명, 급여 조회 (단, 급여는 '\9,000,000' 형식으로 표시)
+SELECT EMP_NAME, TO_CHAR (SALARY, 'L999,999,999')
+FROM EMPLOYEE;
+
 -- 21. EMPLOYEE테이블에서 직원 명, 부서코드, 생년월일, 나이(만) 조회
 -- (단, 생년월일은 주민번호에서 추출해서 00년 00월 00일로 출력되게 하며
 --  나이는 주민번호에서 출력해서 날짜데이터로 변환한 다음 계산)
+SELECT EMP_NAME, DEPT_CODE, TO_CHAR(TO_DATE(TO_NUMBER(SUBSTR(EMP_NO,1,6))), 'YY"년" MM"월" DD"일"') 생년월일,
+FLOOR(MONTHS_BETWEEN(SYSDATE, TO_DATE((SUBSTR(EMP_NO,1,6)), 'RRMMDD'))/12) "나이(만)"
+FROM EMPLOYEE;
+
+
 -- 22. EMPLOYEE테이블에서 부서코드가 D5, D6, D9인 사원만 조회하되 D5면 총무부, D6면 기획부, D9면 영업부로 처리
---  (단, 부서코드 오름차순으로 정렬)
+--  (단, 부서코드 오름차순으로 정렬) 푸는거 아님
+
+
 -- 23. EMPLOYEE테이블에서 사번이 201번인 사원명, 주민번호 앞자리, 주민번호 뒷자리, 
 --  주민번호 앞자리와 뒷자리의 합 조회
+SELECT EMP_NAME, SUBSTR (EMP_NO, 1, 6) AS "주민번호 앞자리",  SUBSTR (EMP_NO, 8) AS "주민번호 뒷자리" , (SUBSTR (EMP_NO, 1, 6)+ SUBSTR (EMP_NO, 8)) AS "앞 뒤자리 합"
+FROM EMPLOYEE
+WHERE EMP_ID = '201';
+
+
 -- 24. EMPLOYEE테이블에서 부서코드가 D5인 직원의 보너스 포함 연봉 합 조회
+/*
+SELECT ((SALARY * BONUS) + SALARY) AS 보너스포함연봉
+FROM EMPLOYEE
+WHERE DEPT_CODE = 'D5'; 푸는 게 아님
+*/
+
 -- 25. EMPLOYEE테이블에서 직원들의 입사일로부터 년도만 가지고 각 년도별 입사 인원수 조회
 --  전체 직원 수, 2001년, 2002년, 2003년, 2004년
+SELECT COUNT(DECODE(TO_CHAR(HIRE_DATE,'YYYY'),'2001',1))||'명' "2001년"
+     , COUNT(DECODE(TO_CHAR(HIRE_DATE,'YYYY'),'2002',1))||'명' "2002년"
+     , COUNT(DECODE(TO_CHAR(HIRE_DATE,'YYYY'),'2003',1))||'명' "2003년"
+     , COUNT(DECODE(TO_CHAR(HIRE_DATE,'YYYY'),'2004',1))||'명' "2004년" 
+     , COUNT(TO_CHAR(HIRE_DATE,'YYYY'))||'명' "전체직원수" 
+FROM EMPLOYEE;
