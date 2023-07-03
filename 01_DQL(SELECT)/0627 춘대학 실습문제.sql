3cced863b1e97350c1b25d6e1c593c6353778514
potato <aviationlovers@naver.com>
2023-07-03 21:31:28 +0900

    commit

--- 01_DQL(SELECT)/0627 춘대학 실습문제.sql
+++ 01_DQL(SELECT)/0627 춘대학 실습문제.sql
@@ -339,3 +339,23 @@ WHERE CATEGORY = (SELECT CATEGORY
                     WHERE DEPARTMENT_NAME = '환경조경학과')
 AND CLASS_TYPE LIKE '전공%' 
 GROUP BY DEPARTMENT_NAME ORDER BY DEPARTMENT_NAME;
+
+-- 1. 계열 정보를 저장한 카테고리 테이블을 만들려고 한다. 다음과 같은 테이블을 작성하시오.
+CREATE TABLE TB_CATEGORY(
+    NAME VARCHAR2(10),
+    USE_YN CHAR(1) DEFAULT 'Y'
+);
+
+-- 2. 과목 구분을 저장한 테이블을 만들려고 한다. 다음과 같은 테이블을 작성하시오.
+CREATE TABLE TB_CLASS_TYPE(
+    NO VARCHAR2(5) PRIMARY KEY,
+    NAME VARCHAR2(10)
+);
+
+-- 3. TB_CATAGORY 테이블의 NAME 컬럼에 PRIMARY KEY 를 생성하시오.
+-- (KEY 이름을 생성하지 않아도 무방함. 만일 KEY 이를 지정하고자 한다면 이름은 본인이 알아서 적당한 이름을 사용한다.)
+ALTER TABLE TB_CATEGORY ADD PRIMARY KEY(NAME);
+
+-- 4. TB_CLASS_TYPE 테이블의 NAME 컬럼에 NULL 값이 들어가지 않도록 속성을 변경하시오.
+ALTER TABLE TB_CLASS_TYPE MODIFY NAME NOT NULL;
+
