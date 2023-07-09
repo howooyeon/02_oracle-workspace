09fc714171648b8eda2cb343d5e5a42f1ed18ca7
potato <aviationlovers@naver.com>
2023-07-10 00:57:13 +0900

    commit

--- 실습문제/0627 춘대학 실습문제.sql
+++ 실습문제/0627 춘대학 실습문제.sql
@@ -1,7 +1,5 @@
 -- 일단 저장
 
--- 하기싫다...ㅠㅜ
-
 SELECT * FROM TB_DEPARTMENT;
 SELECT * FROM TB_STUDENT;
 SELECT * FROM TB_CLASS;
@@ -92,10 +90,15 @@ WHERE PROFESSOR_NAME LIKE '____%' OR PROFESSOR_NAME NOT LIKE '___%';
 -- 3. 춘 기술대학교의 남자 교수들의 이름과 나이를 출력하는 SQL 문장을 작성하시오.
 -- 단, 이때 나이가 적은 사람에서 많은 사람 순서로 화면에 출력되도록 맊드시오. (단, 교수 중
 -- 2000년 이후 출생자는 없으며 출력 헤더는 "교수이름", "나이"로 핚다. 나이는 ‘만’으로 계산한다.)
+SELECT PROFESSOR_NAME 교수이름, EXTRACT(YEAR FROM SYSDATE ) - TO_NUMBER(19 || SUBSTR(PROFESSOR_SSN,1,2)) 나이
+FROM TB_PROFESSOR
+WHERE SUBSTR(PROFESSOR_SSN,8,1) = 1
+ORDER BY SUBSTR(PROFESSOR_SSN,1,2) DESC;
+
 
 -- 4. 교수들의 이름 중 성을 제외한 이름만 출력하는 SQL 문장을 작성하시오. 출력 헤더는?이름? 이 찍히도록 핚다. (성이 2 자인 경우는 교수는 없다고 가정하시오)
 SELECT SUBSTR (PROFESSOR_NAME, 2) AS "이름"
-FROM TB_PROFESSOR; --?
+FROM TB_PROFESSOR;
 
 
 -- 5. 춘 기술대학교의 재수생 입학자를 구하려고 한다. 어떻게 찾아낼 것인가?  
